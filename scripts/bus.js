4c6569d34f5546d468f1d75a36725be2bb5a7234
ayathree <nobanitaayathree333@gmail.com>
2024-02-18 15:02:44 +0600

    added the seat add part

--- scripts/bus.js
+++ scripts/bus.js
@@ -17,10 +17,36 @@ function showSection() {
 
 // all seat access
 const allSeat = document.getElementsByClassName('bg-slate-300 rounded-xl text-center p-5')
+let seatCount = 0;
+let seatLeft = 16;
 for (const seat of allSeat ) {
     seat.addEventListener('click', function (event) {
+
+         seatCount = seatCount + 1;
+         setInnerText('seat-add', seatCount);
+         seatLeft = seatLeft -1;
+         setInnerText('seat-left', seatLeft);
         const seatName = event.target.id
-        setBackgroundColor(seatName)
+        setBackgroundColor(seatName);
+        const seatPrice = document.getElementById('seat-price').innerText;
+        const seatClass = 'economy';
+        const seatTextArea = document.getElementById('seat-text-area');
+         const p1 =document.createElement('p');
+         p1.innerText = seatName;
+        const p2 = document.createElement('p');
+        p2.innerText = seatClass;
+        const p3 = document.createElement('p');
+        p3.innerText = seatPrice;
+        
+         seatTextArea.appendChild(p1);
+        seatTextArea.appendChild(p2);
+        seatTextArea.appendChild(p3);
+        
+
+        
+
+        
+
     })
     
     
